4bcb23e0764a726b53e89ddc5b1280cc29b51bbb
gilangif <gilangidulfitri1201@gmail.com>
2025-08-30 21:20:42 +0700

    initial

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const API = "http://localhost:80"
+export const API = "http://128.199.70.155:80"
 export const API_V2 = "http://128.199.70.155:3000"
 export const DEFAULT_IMAGE = "https://imgcdn.stablediffusionweb.com/2024/11/1/f3db309c-2c1f-45a1-a15a-c078ede00cd7.jpg"
